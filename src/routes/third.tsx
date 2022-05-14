12139f73c043d33db8949d4f47a9a9daf9e07cf9
Grant Kiely <gkiely@crunchyroll.com>
2022-05-14 16:49:09 -0500

    wip

--- src/routes/third.tsx
+++ src/routes/third.tsx
@@ -16,7 +16,79 @@ type Realm = {
 const createRealm = (realm: Realm): Realm['render'] => realm.render;
 
 /**
- * Name: realm-ui
+ * @note: I like this because it follows the game ano
+ * import { createEngine } from '@reframe';
+ * const engine = createEngine({
+ *   state: '',
+ *   render: (state) => {},
+ *   update: (state) => {},
+ *   async effects: (state) => {},
+ *   onLeave: (state) => {},
+ * });
+ * 
+ * engine.update(state); // Returns json
+ * engine.update(state); // Returns json
+ * engine.reset();
+ * 
+ */
+
+/**
+ * import { createEngine } from '@reframe';
+ * const engine = createEngine({
+ *   state: '',
+ *   render: (state) => {},
+ *   update: (state) => {},
+ *   async effects: (state) => {},
+ *   onLeave: (state) => {},
+ * });
+ */
+
+
+/**
+ * Names:
+ * realm
+ * mvu
+ * state, render, update, effects, onLeave
+ */
+
+/**
+ * 
+ * 
+ * 
+ * Routes
+ * For complex screens, you create sub routes
+ * - Sub routes are executed first and then rendered in the parent
+ * - Because sub routes have access to the route state, they can modify it just like a route can
+ * - They can also be provided specific parts of the route state if needed so they can only modify certain parts
+ *
+ * - I want a development feature. I got to:
+ * - /component/MyComponent
+ * - I should be able to modify props or state of the realm there and it is reflected in the component
+ */
+
+/**
+ * You can think of a Realm as a container for your UI's business logic, state and side effects.
+ * A Realm accepts actions, processes state, performs side effects and returns a JSON representation of your UI.
+ *
+ * Basic premise
+ * - If you move all your UI conditionals, state, side effects (and eve routing) out side of your view, you're left with simple stateless components
+ * This makes your UI more reusable, easier to maintain, scalable and easier to test
+ *
+ *
+ * A unidirectional state management library for react, based on the mvu pattern (model-view-update)
+ * - By moving UI conditionals, state, effects (and even routing) outside of your view, you make your UI more reusable and scalable.
+ *
+ * Decouple your UI and business logic from your application
+ *
+ *
+ * Current approach is UI drives the state
+ * - Some condition is met in the UI (event handler or state)
+ * - Perform state update or side effect
+ * - UI is updated to reflect new state
+ *
+ * New approach - State drives the UI
+ * - All interactions are fed to Realm
+ * - Realm processes state and updates UI
  */
 
 /**
@@ -24,14 +96,14 @@ const createRealm = (realm: Realm): Realm['render'] => realm.render;
  * - Managing react state and effects can be cumbersome and non-intuitive
  *
  * Solution:
- * An elm inspired approach to React
+ * An elm inspired state management library for React
  *
  * Route
- * state - the state of your application
- * render - a way to turn your state into a UI structure (json)
- * update - a way to update your state based on actions
+ * - state - the state of your application, the only thing that can mutate
+ * - render - a way to turn your state into UI (json)
+ * - update - a way to update your state based current state and actions
  * * effects - a way to update your state based on external events
- * * onLeave - a way to update your state based on leaving a route
+ * * onLeave - a way to update your state when leaving a route
  *
  * * effects and onLeave are optional
  *
@@ -45,7 +117,8 @@ const createRealm = (realm: Realm): Realm['render'] => realm.render;
  * 3 properties (state, view, reducer) + 2 optional (effects, onLeave)
  * - Optionally use `machine` instead of reducer, state and effects
  * - when machine is used, view gets called with 2 parameters (state, current)
- * view returns json
+ *
+ * Render function is pure, it accepts a state object and returns json
  * should be able to run in a service worker or a node instance (for SSR and portability)
  *
  * Any changes to state in the update or the effects function update the UI automatically
@@ -80,6 +153,25 @@ const createRealm = (realm: Realm): Realm['render'] => realm.render;
  *    - Rendering html is an implementation detail, just as rendering a SPA application on the client is a UI implementation detail
  *      - The user does not care if you use server rendered HTML, React, Vue or any other library.
  *      - Using JSON allows for UI implentation flexibility. Either server render or use a library, it is up to you.
+ *
+ *
+ * Realm replaces: Redux, React-router, useEffect, useState and useReducer.
+ * Additional notes:
+ * - Replaces redux, hooks, react-router
+ *
+ * - At the top level you define a:
+ * <Realm routes={routes} />
+ */
+
+/**
+ * Personal notes
+ * Who is my target audience?
+ * - Anyone who is fed up with the current state management system of react or organization of components
+ * - Anyone who was an elm developer
+ *
+ *
+ * I'm gonna have a crack at an elm style react state mgmt library. No react-router, redux or hooks needed. Just a single object with the properties (state, render, update, effects). Outputs json and maps to components.
+ *
  */
 
 const button = (text: string, action = { type: 'input', payload: text }) => {
@@ -180,7 +272,7 @@ export const isValidInput = (input: string): boolean => {
   return true;
 };
 
-export default createRealm({
+const third = createRealm({
   state: {
     input: '',
   },
@@ -220,3 +312,7 @@ export default createRealm({
     state.input = 'left';
   },
 });
+
+export default {
+  '/third': third,
+};
